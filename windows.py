067926cab124733d7fa94bbaf408efc8c366eae7
zhang-da-miao <2859662250@qq.com>
2020-08-22 23:33:50 +0800

    new

--- windows.py
+++ windows.py
@@ -1,17 +1,20 @@
 import pygame
 import sys
+from settings import Settings
 
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_set = Settings()
+    screen = pygame.display.set_mode((ai_set.screen_width, ai_set.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        screen.fill(ai_set.bg_color)
         pygame.display.flip()
 
-print("hello world")
+
 run_game()
